afd06768b137dba1834561fe910c3bcb90b072d9
DanDiazRep <ddiaz@servex-us.com>
2020-04-01 11:22:18 -0500

    04012020

---
+++ Training1/Magnuson-Umea/info.js
@@ -0,0 +1,11 @@
+﻿
+var umeaInfo = {
+    "defaultLayers": [
+        "Single_General1",
+        "Single_Key",
+        "Single_Label1",
+        "Single_SidePanel1",
+        "Single_Top1"
+    ]
+
+}
--- Training1/Magnuson-Umea/productBuilder.js
+++ Training1/Magnuson-Umea/productBuilder.js
@@ -13,33 +13,54 @@ camera.setTarget(BABYLON.Vector3.Zero());
 var baseUrl;
 var asyncMesh;
 var currentMesh;
-//scene.clearColor = new BABYLON.Color3(0.95, 0.95, 0.95); //Background color
+scene.clearColor = new BABYLON.Color3(0, 0, 0); //Background color
 
 
 var createScene = function () {
-
 
     // Model loader
     BABYLON.GLTFFileLoader.IncrementalLoading = true;
     baseUrl = "/assets/models/";
     lowFile = "umea.glb";
-    BABYLON.SceneLoader.ImportMesh("", baseUrl, lowFile, scene, function (syncMesh) {
+
+    BABYLON.SceneLoader.ImportMesh("", baseUrl, lowFile, scene, function (meshes) {
 
         // Camera 
-        scene.createDefaultCameraOrLight(true, true, true);        
+        scene.createDefaultCameraOrLight(true, true, true);
         scene.activeCamera.alpha = Math.PI / 3;
         scene.activeCamera.beta = 7 * Math.PI / 16;
         scene.activeCamera.radius = 5000;
-
-
+        scene.activeCamera.panningSensibility = 5;
 
         //Lightning
         var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 2, 0), scene);
-        light.intensity = 0.8;
+        light.intensity = 0.5;
 
 
-        //Env texture
-        var hdrTexture = new BABYLON.CubeTexture.CreateFromPrefilteredData("assets/environment/city.dds", scene);
+        for (var i = 1; i < meshes.length; i++) {
+
+            meshes[i].actionManager = new BABYLON.ActionManager(scene);
+            meshes[i].actionManager.registerAction(new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPointerOverTrigger, function(ev) {
+            }, false));
+
+            meshes[i].isVisible = umeaInfo.defaultLayers.includes(meshes[i].name);
+
+            meshes[i]._material.albedoColor.r = 0;
+            meshes[i]._material.albedoColor.g = 0.3;
+            meshes[i]._material.albedoColor.b = 0.9;
+            //meshes[i]._material.albedoColor = new BABYLON.Color3.FromHexString("#FF4444");
+
+            console.log(meshes[i]);
+
+          /*  if (meshes[i].name == "Single_SidePanel1") {
+                meshes[i].position = new BABYLON.Vector3(0, 2, 0);
+                meshes[i].rotation = new BABYLON.Vector3(Math.PI / 4, 0, 0);
+            }*/
+
+    }
+
+
+        var hdrTexture = new BABYLON.CubeTexture.CreateFromPrefilteredData("assets/environment/room.dds", scene);
         scene.environmentTexture = hdrTexture;
 
     });
